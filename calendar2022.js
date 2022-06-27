73809e567e453c1f469c1050c77c8a1c988cc88a
mrsfriedmsh <99189153+mrsfriedmsh@users.noreply.github.com>
2022-06-26 19:33:10 -0700

    Add files via upload

---
+++ calendar2022.js
@@ -0,0 +1,222 @@
+export const CALENDAR = [
+    {
+        id: 0,
+        round: 'Round 1',
+        raceName: 'Formula 1 Gulf Air Bahrain Grand Prix 2022',
+        image: '',
+        location: 'Bahrain',
+        date: 'March 18-20, 2022',
+        circuitname: 'Bahrain International Circuit',
+        description: 'Night race, 5.412 km, 57 laps, 15 turns, 3 DRS zones'
+    },
+    {
+        id: 1,
+        round: 'Round 2',
+        raceName: 'Formula 1 STC Saudia Arabian Grand Prix 2022',
+        image: '',
+        location: 'Saudi Arabia',
+        date: "March 25-27, 2022",
+        circuitName: 'Jeddah Corniche Circuit',
+        description: 'Night Street race, 6.174 km, 50 laps, 27 turns, 3 DRS zones'
+    },
+    {
+        id: 2,
+        round: 'Round 3',
+        raceName:'Formula 1 Heineken Australian Grand Prix 2022',
+        image: '',
+        location: 'Australia',
+        date: 'April 8-10, 2022',
+        circuitName: 'Albert Park Circuit',
+        description: 'Day Street race, 5.278 km, 58 laps, 14 turns, 2 DRS zones'
+    },
+    {
+        id: 3,
+        round: 'Round 4',
+        raceName: 'Formula 1 Rilox Gran Premio Del Made in Itialy E DellEmilia-Romagna 2022',
+        image: '',
+        location: 'Italy',
+        date: 'April 22-24, 2022',
+        circuitName: 'Autodromo Enzo e Dina Ferrari',
+        description: 'Day race, 4.909 km, 63 lap, 19 turns, 1 DRS zone'
+    },
+    {
+        id: 4,
+        round: 'Round 5',
+        raceName: 'Formlua 1 Crypto.com Miami Grand Prix 2022',
+        image: '',
+        location: 'United States',
+        date: 'May 6-8, 2022',
+        circuitName: 'Miami International Autodrome',
+        description: 'Day Street race, 5.412 km, 57 laps, 19 turns, 3 DRS zones'
+    },
+    {
+        id: 5,
+        round: 'Round 6',
+        raceName: 'Formula 1 Pirelli Gran Premio De Espana 2022',
+        image: '',
+        location: 'Spain',
+        date: 'May 20-22, 2022',
+        circuitName: 'Circuit de Barcelona-Catalunya',
+        description: 'Day race, 4.675 km, 66 laps, 16 turns, 2 DRS zones'
+    },
+    {
+        id: 6,
+        round: 'Round 7',
+        raceName: 'Formula 1 Grand Prix De Monaco 2022',
+        image: '',
+        location: 'Monaco',
+        date: 'May 27-29, 2022',
+        circuitName: 'Circuit de Monaco',
+        description: 'Day Street race, 3.337 km, 78 laps, 19 turns, 1 DRS zone'
+    },
+    {
+        id: 7,
+        round: 'Round 8',
+        raceName: 'Formula 1 Azerbaijan Grand Prix 2022',
+        image: '',
+        location: 'Azerbaijan',
+        date: 'June 10-12, 2022',
+        circuitName: 'Baku City Circuit',
+        description: 'Day Street race, 6.003 km, 51 laps, 20 turns, 2 DRS zones'
+    },
+    {
+        id: 8,
+        round: 'Round 9',
+        raceName: 'Formula 1 AWS Grand Prix Du Canada 2022',
+        image: '',
+        location: 'Canada',
+        date: 'June 17-19, 2022',
+        circuitName: 'Circuit Gilles-Villeneuve',
+        description: 'Day race, 4.361 km, 70 laps, 14 turns, 2 DRS zones'
+    },
+    {
+        id: 9,
+        round: 'Round 10',
+        raceName: 'Formula 1 Lenovo British Grand Prix 2022',
+        image: '',
+        location: 'Grand Britian',
+        date: 'July 1-3, 2022',
+        circuitName: 'Silverstone Circuit',
+        description: 'Day Race, 5.891 km, 52 laps, 18 turns, 2 DRS zones'
+    },
+    {
+        id: 10,
+        round: 'Round 11',
+        raceName: 'Formla 1 Rolex Grosser Preis Von Osterreich 2022',
+        image: '',
+        location: 'Austria',
+        date: 'July 8-10',
+        circuitName: 'Red Bull Ring',
+        description: 'Day Race, 4.318 km, 71 laps, 10 turns, 3 DRS zones'
+    },
+    {
+        id: 11,
+        round: 'Round 12',
+        raceName: 'Formula 1 Lenovo Grand Prix De France 2022',
+        image: '',
+        location: 'France',
+        date: 'July 22-24, 2022',
+        circuitName: 'Circuit Paul Ricard',
+        description: 'Day Race, 5.842 km, 53 laps, 15 turns, 2 DRS zones'
+    },
+    {
+        id: 12,
+        round: 'Round 13',
+        raceName: 'Formula 1 Aramco Magyar Nagydij 2022',
+        image: '',
+        location: 'Hungary',
+        date: 'July 29-31, 2022',
+        circuitName: 'Hungaroring',
+        description: 'Day Race, 4.381 km, 70 laps, 14 turns, 1 DRS zone'
+    },
+    {
+        id: 13,
+        round: 'Round 14',
+        raceName: 'Formula 1 Rolex Belgian Grand Prix 2022',
+        image: '',
+        location: 'Belgium',
+        date: 'August 26-28, 2022',
+        circuitName: 'Circuit de Spa-Francorchamps',
+        description: 'Day Race, 7.004 km, 44 laps, 18 turns, 2 DRS zones'
+    },
+    {
+        id: 14,
+        round: 'Round 15',
+        raceName: 'Formula 1 Heineken Dutch Grand Prix 2022',
+        image: '',
+        location: 'Netherlands',
+        date: 'September 2-4, 2022',
+        circuitName: 'Circuit Zandoort',
+        description: 'Day Race, 4.259 km, 72 laps, 14 turns, 2 DRS zones'
+    },
+    {
+        id: 15,
+        round: 'Round 16',
+        raceName: 'Formula 1 Pirelli Gran Premio DItalia 2022',
+        image: '',
+        location: 'Monza, Italy',
+        date: 'September 9-11, 2022',
+        circuitName: 'Autodromo Nazionale Monza',
+        description: 'Day Race, 5.793 km, 53 laps, 11 turns, 2 DRS zones'
+    },
+    {
+        id: 16,
+        round: 'Round 17',
+        raceName: 'Formula 1 Singapore Airlines Singapore Grand Prix 2022',
+        image: '',
+        location: 'Singapore',
+        date: 'September 30 - October 2, 2022',
+        circuitName: 'Marina Bay Street Circuit',
+        description: 'Night Street Race, 5.063 km, 61 laps, 23 turns, 3 DRS zones'
+    },
+    {
+        id: 17,
+        round: 'Round 18',
+        raceName: 'Formula 1 Honda Japanese Grand Prix 2022',
+        image: '',
+        location: 'Japan',
+        date: 'October 7-9, 2022',
+        circuitName: 'Suzuka International Racing Course',
+        description: 'Day Race, 5.807 km, 53 laps, 18 turns, 1 DRS zone'
+    },
+    {
+        id: 18,
+        round: 'Round 19',
+        raceName: 'Formula 1 Aramco United States Grand Prix 2022',
+        image: '',
+        location: 'United States',
+        date: 'October 21-23, 2022',
+        circuitName: 'Circuit of The Americas',
+        description: 'Day Race, 5.513 km, 56 laps, 20 turns, 2 DRS zones'
+    },
+    {
+        id: 19,
+        round: 'Round 20',
+        raceName: 'Formula 1 Gran Premio De La Ciudad De Mexico 2022',
+        image: '',
+        location: 'Mexico',
+        date: 'October 28-30, 2022',
+        circuitName: "Autodromo Hermanos Rodriguez",
+        description: 'Day Race, 4.304 km, 71 laps, 17 turns, 2 DRS zones'
+    },
+    {
+        id: 20,
+        round: 'Round 21',
+        raceName: 'Formula 1 Heineken Grande Premio De Sao Paulo 2022',
+        image: '',
+        location: 'Brazil',
+        date: 'November 11-13, 2022',
+        circuitName: 'Autodromo Jose Carlos Pace',
+        description: 'Day Race, 4.309 km, 71 laps, 15 turns, 2 DRS zones'
+    },
+    {
+        id: 21,
+        round: 'Round 22',
+        raceName: 'Formula 1 Etihad Airways Abu Dhabi Grand Prix 2022',
+        image: '',
+        location: 'Abu Dhabi',
+        date: 'November 18-20, 2022',
+        circuitName: 'Yas Marina Circuit',
+        description: 'Night Race, 5.281 km, 58 laps, 16 turns, 2 DRS zones'
+    }
+];
---
+++ schedule.js
@@ -0,0 +1,8 @@
+export const SCHEDULE = [
+    {
+        id: 21,
+        racename: 'Formula 1 Etihad Airways Abu Dhabi Grand Prix 2022',
+        
+        
+    }
+]
